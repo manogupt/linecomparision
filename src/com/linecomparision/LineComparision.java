342f0cbe7beb1bb46ef4de23f3f0783dea400b91
manogupt <manojgupta123929@gmail.com>
2022-07-08 08:39:29 +0530

    Added :  Compare two length

--- src/com/linecomparision/LineComparision.java
+++ src/com/linecomparision/LineComparision.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class LineComparision {
 
 	public static void main(String[] args) {
-		{
+	    {
 			double x1, x2, y1, y2, LengthOfLine , LengthOfLine2 ;  //Declaration 
 			
 			Scanner scanner = new Scanner(System.in);
@@ -24,7 +24,7 @@ public class LineComparision {
 			y2 = scanner.nextDouble();
 			
 			LengthOfLine = Math.sqrt(  (Math.pow(x2-x1 , 2))  + (Math.pow(y2-y1 , 2)) );
-			System.out.println(LengthOfLine);
+			System.out.println("Length of Line 1:" + LengthOfLine);
 			
 			
 			System.out.println("Enter the value of x1 : ");
@@ -40,14 +40,13 @@ public class LineComparision {
 			y2 = scanner.nextDouble(); 
 			
 			LengthOfLine2 = Math.sqrt(  (Math.pow(x2-x1, 2))  + (Math.pow(y2-y1, 2)));
-			System.out.println(LengthOfLine2);
+			System.out.println("Length of Line 1:" + LengthOfLine2);
 			
 			String s1,s2;
 			s1 = Double.toString(LengthOfLine);
 			s2 = Double.toString(LengthOfLine2);
-			System.out.println("Equality of two length is " +s1.equals(s2));
+			System.out.println("Equality of two lines using Equal Method : " +s1.equals(s2));
+			System.out.println("Compare of two length is :" + s1.compareTo(s2));
 		}
 	}
 }
-
-	
