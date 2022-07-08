08aed52345cc8560022b538f9dec84e0b06f36f8
manogupt <manojgupta123929@gmail.com>
2022-07-08 08:30:19 +0530

    Added : Equality of two length

--- src/com/linecomparision/LineComparision.java
+++ src/com/linecomparision/LineComparision.java
@@ -6,29 +6,48 @@ public class LineComparision {
 
 	public static void main(String[] args) {
 		{
-		    {
-				double x1, x2, y1, y2, LengthOfLine;  //Declaration 
-
-				Scanner scanner = new Scanner(System.in);
-
-				// taking input from user
-				System.out.println("Enter the value of x1 :");
-				x1 = scanner.nextDouble();
-
-				System.out.println("Enter the value of x2 :");
-				x2 = scanner.nextDouble();
-
-				System.out.println("Enter the value of y1 :");
-				y1 = scanner.nextDouble();
-
-				System.out.println("Enter the value of y2 :");
-				y2 = scanner.nextDouble();
-
-				LengthOfLine = Math.sqrt(  (Math.pow(x2-x1,2))  + (Math.pow(y2-y1,2)) );
-
-				System.out.println(LengthOfLine);
-			}
+			double x1, x2, y1, y2, LengthOfLine , LengthOfLine2 ;  //Declaration 
+			
+			Scanner scanner = new Scanner(System.in);
+			
+			// taking input from user
+			System.out.println("Enter the value of x1 :");
+			x1 = scanner.nextDouble();
+			
+			System.out.println("Enter the value of x2 :");
+			x2 = scanner.nextDouble();
+			
+			System.out.println("Enter the value of y1 :");
+			y1 = scanner.nextDouble();
+			
+			System.out.println("Enter the value of y2 :");
+			y2 = scanner.nextDouble();
+			
+			LengthOfLine = Math.sqrt(  (Math.pow(x2-x1 , 2))  + (Math.pow(y2-y1 , 2)) );
+			System.out.println(LengthOfLine);
+			
+			
+			System.out.println("Enter the value of x1 : ");
+			x1 = scanner.nextDouble(); 
+			
+			System.out.println("Enter the value of x2 : ");
+			x2 = scanner.nextDouble(); 
+			
+			System.out.println("Enter the value of y1 : ");
+			y1 = scanner.nextDouble(); 
+			
+			System.out.println("Enter the value of y2 : ");
+			y2 = scanner.nextDouble(); 
+			
+			LengthOfLine2 = Math.sqrt(  (Math.pow(x2-x1, 2))  + (Math.pow(y2-y1, 2)));
+			System.out.println(LengthOfLine2);
+			
+			String s1,s2;
+			s1 = Double.toString(LengthOfLine);
+			s2 = Double.toString(LengthOfLine2);
+			System.out.println("Equality of two length is " +s1.equals(s2));
 		}
 	}
-
 }
+
+	
